e09b7dba40f048acd66d995a560475993e4df648
조 승준 <tmdwns489@gmail.com>
2023-10-06 02:57:03 +0900

    DESIGN: cart페이지 디자인 수정 기능 추가

--- main/main.js
+++ main/main.js
@@ -1,29 +1,29 @@
-var slideIndex = 0; // 현재 슬라이드 인덱스
-var slideInterval = 2000; // 슬라이드 전환 간격 (밀리초)
+let SlideIndex = 0; // 현재 슬라이드 인덱스
+let SlideInterval = 2000; // 슬라이드 전환 간격 (밀리초)
 
-function showSlides() {
-  var slides = document.getElementsByClassName("mySlides");
-  var size = slides.length;
+function ShowSlides() {
+  let Slides = document.getElementsByClassName("mySlides");
+  let Size = Slides.length;
 
   // 현재 슬라이드를 숨김
-  for (var i = 0; i < size; i++) {
-    slides[i].style.display = "none";
+  for (var i = 0; i < Size; i++) {
+    Slides[i].style.display = "none";
   }
 
   // 다음 슬라이드로 이동
-  slideIndex++;
-  if (slideIndex >= size) {
-    slideIndex = 0; // 처음 슬라이드로 이동
+  SlideIndex++;
+  if (SlideIndex >= Size) {
+    SlideIndex = 0; // 처음 슬라이드로 이동
   }
 
   // 현재 슬라이드를 표시
-  slides[slideIndex].style.display = "block";
+  Slides[SlideIndex].style.display = "block";
 
   // 다음 슬라이드 전환을 예약
-  setTimeout(showSlides, slideInterval);
+  setTimeout(ShowSlides, SlideInterval);
 }
 
 // 페이지 로드 후 슬라이드 쇼 시작
 window.onload = function () {
-  showSlides(); // 초기 슬라이드 표시
+  ShowSlides(); // 초기 슬라이드 표시
 };
