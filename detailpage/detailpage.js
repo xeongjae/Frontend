0288f34cb82bfa9fe186992abeef004313b463a9
jinsupark4255 <jin04007@naver.com>
2023-10-06 20:12:25 +0900

    상세페이지 기능구현

--- detailpage/detailpage.js
+++ detailpage/detailpage.js
@@ -1,3 +1,4 @@
+// 이미지 슬라이드 관련 코드
 let currentSlide = 0;
 const slides = document.querySelectorAll(".image_slider img");
 const slideWidth = slides[0].clientWidth;
@@ -19,4 +20,30 @@ document.getElementById("prevSlide").addEventListener("click", () => {
 function slideImages() {
   const slider = document.querySelector(".image_slider");
   slider.style.transform = `translateX(-${currentSlide * slideWidth}px)`;
+}
+
+// 주문 수량 및 가격 업데이트 관련 코드
+const decreaseButton = document.getElementById("decreaseCount");
+const increaseButton = document.getElementById("increaseCount");
+const orderCountMass = document.querySelector(".order_count_mass");
+const orderMoneyMass = document.querySelector(".order_money_mass");
+const unitPrice = 33000; // 단위 가격
+let count = 0;
+
+decreaseButton.addEventListener("click", function () {
+  if (count > 0) {
+    count--;
+    updateCountAndPrice();
+  }
+});
+
+increaseButton.addEventListener("click", function () {
+  count++;
+  updateCountAndPrice();
+});
+
+function updateCountAndPrice() {
+  orderCountMass.textContent = `${count}개`;
+  const totalPrice = count * unitPrice;
+  orderMoneyMass.textContent = `${totalPrice.toLocaleString()}원`; // toLocaleString() 함수로 천 단위마다 쉼표 추가
 }
