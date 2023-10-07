b18e0a356aba9498cf147dbedaa74d33983a90ce
Kim SungJae <newsungjae0407@gmail.com>
2023-10-07 20:14:02 +0900

    FEAT: 회원가입 로그인기능 유효성검사

--- login/login.js
+++ login/login.js
@@ -1,13 +1,52 @@
-// header.html을 header-container에 포함
-fetch("../header_footer/header.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("header-container").innerHTML = data;
-  });
+//로그인기능 
+document.querySelector('.login-form').addEventListener('submit', async function(e) {
+  e.preventDefault();
 
-// footer.html을 footer-container에 포함
-fetch("../header_footer/footer.html")
-  .then((response) => response.text())
-  .then((data) => {
-    document.getElementById("footer-container").innerHTML = data;
-  });
+  const email = document.getElementById('idInput').value;
+  const password = document.getElementById('passwordInput').value;
+  const apiUrl = 'http://kdt-sw-6-team08.elicecoding.com/login';
+
+  if (!email) {
+    return alert('email을 입력하세요.');
+  }
+  // 이메일 형식 검증
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+  if (!emailRegex.test(email)) {
+      return alert('올바른 이메일 형식을 입력하세요.');
+  }
+  if (!password) {
+    return alert('비밀번호를 입력하세요.');
+  }
+
+
+  //서버에 보낼 데이터
+  const loginData = {
+      email,
+      password,
+  };
+  
+
+  //로그인 요청 (Fetch API)
+    try {
+        const response = await fetch(apiUrl, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(loginData)
+        });
+
+        const responseData = await response.json();
+
+        if (response.ok) {
+            localStorage.setItem("token", responseData.token);
+            window.location.href = '/main/index.html';
+        } else {
+            alert("로그인 정보를 확인하세요");
+        }
+
+    } catch (error) {
+        console.error('Error:', error);
+        alert('서버 오류 발생');
+    }
+});
