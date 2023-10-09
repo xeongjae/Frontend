d799d8ba6594c344919caa1e7eed5d868115501f
조 승준 <tmdwns489@gmail.com>
2023-10-09 12:25:28 +0900

    FEAT: mypage 기능 추가

--- mypage/mypage.js
+++ mypage/mypage.js
@@ -1,13 +1,36 @@
+const EmailInput = document.querySelector(".email-input");
+const PwInput = document.querySelector(".pw-input");
+const PwCheckInput = document.querySelector(".pw-check-input");
+const PhoneInput = document.querySelector(".phone-input");
+const Address_1 = document.querySelector(".address-input-first");
+const Address_2 = document.querySelector(".address-input-second");
+const ModifyBtn = document.querySelector(".modify-btn");
+
+fetch("/api/api.json")
+  .then((res) => {
+    if (!res.ok) {
+      throw new Error("Network response was not ok");
+    }
+    return res.json();
+  })
+  .then((data) => {
+    console.log(data);
+    EmailInput.value = data.user[0].email;
+    PhoneInput.value = data.user[0].data.phone;
+    Address_1.value = data.user[0].data.address;
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+//주소 다음 API
 document
-  .querySelector(".find-address-button button is-rounded")
+  .querySelector(".find-address-button")
   .addEventListener("click", function () {
     new daum.Postcode({
       oncomplete: function (data) {
         let addr = "";
         let extraAddr = "";
-
-        const address1Input = document.getElementById("addressInput");
-        const address2Input = document.getElementById("detailAddressInput");
 
         if (data.userSelectedType === "R") {
           addr = data.roadAddress;
@@ -16,7 +39,7 @@ document
         }
 
         if (data.userSelectedType === "R") {
-          if (data.bname !== "" && /[동|로|가]$/g.test(data.bname)) {
+          if (data.bname !== "" && /[동|로|가]$/.test(data.bname)) {
             extraAddr += data.bname;
           }
           if (data.buildingName !== "" && data.apartment === "Y") {
@@ -28,9 +51,36 @@ document
           }
         }
 
-        address1Input.value = `${addr} ${extraAddr}`;
-        address2Input.placeholder = "상세 주소를 입력해 주세요.";
-        address2Input.focus();
+        Address_1.value = `${addr} ${extraAddr}`;
+        Address_2.placeholder = "상세 주소를 입력해 주세요.";
+        Address_2.focus();
       },
     }).open();
   });
+
+PhoneInput.addEventListener("click", function () {
+  this.value = "";
+});
+
+// input 이벤트 리스너를 추가합니다.
+PhoneInput.addEventListener("input", function () {
+  let phoneNumber = this.value.replace(/\D/g, "");
+  if (phoneNumber.length >= 4 && phoneNumber.length <= 7) {
+    phoneNumber = phoneNumber.slice(0, 3) + "-" + phoneNumber.slice(3);
+  } else if (phoneNumber.length > 7) {
+    phoneNumber =
+      phoneNumber.slice(0, 3) +
+      "-" +
+      phoneNumber.slice(3, 7) +
+      "-" +
+      phoneNumber.slice(7, 11);
+  }
+  this.value = phoneNumber;
+});
+
+ModifyBtn.addEventListener("click", function (e) {
+  if (PwInput.value !== PwCheckInput.value) {
+    e.preventDefault();
+    alert("비밀번호를 다시 확인해주세요");
+  }
+});
