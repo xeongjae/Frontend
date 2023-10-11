c046799a463a9134e4d989e6b47d9e379d052770
Kim yeongjun <cosmoyj7733@gmail.com>
2023-10-11 09:43:00 +0900

    DOCS:UI 디자인 피드백 반영

--- manager/category-management/addCategory.js
+++ manager/category-management/addCategory.js
@@ -1,12 +1,18 @@
 const formSubmit = document.getElementById("form");
 const categoryCard = document.querySelector(".category-card");
 const btnsDelete = document.querySelectorAll(".button-delete");
+const itemBoxs = document.querySelectorAll(".item-box p");
 
 
 
 function submitHandler(e) {
   e.preventDefault();
 
+  itemBoxs.forEach((box) => {
+    const text = box.innerHTML;
+
+    console.log(text);
+  })
 
 
   const itemEl = document.createElement("div");
@@ -17,21 +23,16 @@ function submitHandler(e) {
 
 
 
-
   const itemForm = `
     <p>${inputedCategory}</p>
     <button class="button-delete" data-id="${inputedCategory}">삭제</button>
   `
 
   if (inputedCategory.length !== 0) {
-    console.log("GG")
     itemEl.innerHTML = itemForm;
   } else {return;};
 
   categoryCard.prepend(itemEl);
-
-  console.log(inputedCategory)
-  console.log("제출");
 
 
 }
